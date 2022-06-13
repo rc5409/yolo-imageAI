eaa5913e6e50b2627725db8fcb2a68dc322647e8
rc5409 <rashmi.chelliah@gmail.com>
2022-06-13 16:58:56 +0530

    all files commit

---
+++ tiny_yolo.py
@@ -0,0 +1,11 @@
+from imageai.Detection import ObjectDetection
+import os
+execution_path = os.getcwd()    
+detector = ObjectDetection()
+detector.setModelTypeAsTinyYOLOv3()
+detector.setModelPath( os.path.join(execution_path , "yolo-tiny.h5"))
+detector.loadModel()    
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpeg"), output_image_path=os.path.join(execution_path , "imagenew2.jpg"))
+
+for eachObject in detections:
+    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
